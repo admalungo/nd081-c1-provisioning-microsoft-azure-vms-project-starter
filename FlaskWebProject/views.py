e0a115aad38a7765122b817a6668762fc226657e
admalungo <52615957+admalungo@users.noreply.github.com>
2021-05-12 00:52:47 -0400

    Fixed typos

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -62,7 +62,7 @@ def post(id):
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        app.logger.info('admin user logged in successfull - is_authenticated.')
+        # app.logger.info('admin user logged in successfull - is_authenticated.')
         return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
@@ -74,7 +74,7 @@ def login():
             return redirect(url_for('login'))
         # the next 3 lines added by me
         else:
-            app.Logger.info('\'admin\\ user logged in successfuly.')
+            app.Logger.info('\'admin\' user logged in successfuly.')
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
